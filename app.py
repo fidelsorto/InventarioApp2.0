6f5b4459a2524e28906dd746c4a22df522a7bdd4
CarlosEsen2020 <robertosiete2@gmail.com>
2021-08-08 19:53:04 -0600

    Estructura inicial
    Se plantea la estructura inicial del proyecto para que se creen los archivos .py

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session
+from routes.main import MainRoutes
+from routes.register import RegisterRoutes
+from routes.log_process import LogProcessRoutes
+from routes.dashboard import DashboardRoutes
+import bcrypt
 
 app = Flask(__name__)
+app.secret_key = "Bad1secret2key3!+"
 
-@app.route("/")
-def index():
-   return  render_template('login.html')
 
-if __name__ == '__main__':
+MainRoutes.configure_routes(app)
+RegisterRoutes.configure_routes(app)
+LogProcessRoutes.configure_routes(app)
+DashboardRoutes.configure_routes(app)
+
+
+if __name__ == "__main__":
     app.run(debug=True)
