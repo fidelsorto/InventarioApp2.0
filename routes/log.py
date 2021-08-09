7dd4f8dff50207ace917434d97b2abd8687352d6
CarlosEsen2020 <robertosiete2@gmail.com>
2021-08-09 00:43:49 -0600

    insert forms and backend

--- routes/log.py
+++ routes/log.py
@@ -1,5 +1,6 @@
 from flask import request, render_template, redirect, session, flash
 from logic.admin_logic import AdminLogic
+from logic.product_logic import ProdLogic
 class LogRoutes():
     @staticmethod
     def configure_routes(app):
@@ -18,3 +19,26 @@ class LogRoutes():
                     return render_template("login.html")
                 else:
                      return render_template("crud.html")
+
+        @app.route("/insert", methods=["GET", "POST"])
+        def insert():
+            if request.method == "GET":
+                return render_template("crud.html")
+            elif request.method == "POST":
+                sku = request.form["sku"]
+                name = request.form["name"]
+                quantity = request.form["quantity"]
+                price = request.form["price"]
+                size = request.form["size"]
+                color = request.form["color"]
+                category = request.form["category"]
+                photo = None
+
+                Plogic = ProdLogic()
+                rows = Plogic.insertProduct(sku, name, quantity, price, size, color, category, photo)
+
+                if rows > 0:
+                    return render_template("login.html")
+                else:
+                     return render_template("crud.html")
+
